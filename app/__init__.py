9b821535137b3d6bad498cf3db2ad0ce99f8bbed
ze-fernando <jfenginner935@gmail.com>
2023-11-23 19:05:26 -0300

    fix: db created

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
